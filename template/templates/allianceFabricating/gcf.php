254f163a23b4d999bf8cdcb589334c8a69c49049
Matt Elliott <melliott@ccistudios.com>
2012-02-16 15:32:57 -0500

    added google chrome frame for IE users

---
+++ template/templates/allianceFabricating/gcf.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+
+<head>
+<link rel="stylesheet" href="/templates/<?= $this->template ?>/css/gcf.css">
+	
+</head>
+
+<body>
+		
+		<div class="logo">
+			<img src="templates/<?= $this->template ?>/images/gcfLogo.jpg" width="284" height="54" alt="Google Chrome Frame">
+		</div>
+		
+		<h3>Make your browser more powerful</h3>
+		<p>
+			Google Chrome Frame is a free plug-in for Internet Explorer. Some advanced web apps use Google Chrome Frame to provide you with additional features and better performance. 
+		</p>
+		
+		<ul>
+			<li>Speed up your web experience</li>
+			<li>Enjoy more advanced features</li>
+			<li>Free!</li>
+		</ul>
+		
+		<br/>
+		<a href="http://google.com/chromeframe/eula.html">Get Google Chrome Frame Here</a>
+		
+</body>
+</html>
